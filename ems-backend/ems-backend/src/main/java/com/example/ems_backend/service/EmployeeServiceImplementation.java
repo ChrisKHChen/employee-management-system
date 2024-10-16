9833ebc2205a6aa6032b8d563fa264a6512e7f6d
Chris Chen <chris.chen1@ontariotechu.net>
2024-10-16 02:52:33 -0400

    Employee attribute update + scaffold service layer

--- ems-backend/ems-backend/src/main/java/com/example/ems_backend/service/EmployeeServiceImplementation.java
+++ ems-backend/ems-backend/src/main/java/com/example/ems_backend/service/EmployeeServiceImplementation.java
@@ -1,4 +1,40 @@
 package com.example.ems_backend.service;
 
-public class EmployeeServiceImplementation{
+import com.example.ems_backend.model.Employee;
+import com.example.ems_backend.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeServiceImplementation implements EmployeeService{
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public Employee createEmployee(Employee employee){
+        return null;
+    }
+
+    @Override
+    public Optional<Employee> getEmployeeById(Long id){
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Employee> getAllEmployees(){
+        return List.of();
+    }
+
+    @Override
+    public Employee updateEmployee(Long id, Employee employee){
+        return null;
+    }
+
+    @Override
+    public void deleteEmployee(Long id){
+
+    }
 }
